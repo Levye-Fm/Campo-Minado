850d46a30c7a16be2853f63b6f31ab3955e03228
Levye-Fm <levi.ferreira08@aluno.ifce.edu.br>
2025-02-05 19:33:20 -0300

    comentários

--- script.js
+++ script.js
@@ -1,18 +1,25 @@
+// variavel cronometro tem que ser criada fora da função para evitar bug na hora de resetar o cronometro
 var cronometro;
+
 function calcularTempo() {
+    //variaveis
     var segundos = 0;
     var minutos = 0;
+    //span que representa aonde o cronometro está
     var tempo = document.getElementById('tempo');
-    
+    //para o cronometro(para caso um cronometro já esteja rodando e o usuário comece outro jogo, reseta o cronometro)
     clearInterval(cronometro);
+
+    //a cada 1000 milissegundos, aumenta um segundo e atualiza o span do cronometro
     cronometro = setInterval(function() {  
       segundos++;
       
-      if (segundos == 60) {
+      if (segundos == 60) { // a cada 60 segundos, aumenta um minuto e volta os segundos para o valor 0
         segundos = 0;
         minutos++
       }
-      if (segundos < 10) {
+      if (segundos < 10) { //deixa o cronometro mais bonito, evitando valores como 1:1(1 minuto e 1 segundo)
+        //transformando-os em 1:01 por exemplo
         tempo.innerHTML = minutos+":0"+segundos;
       } else {tempo.innerHTML = minutos+":"+segundos;}
     }
@@ -21,4 +28,6 @@ function calcularTempo() {
             
   }
   
-  function newGame() {}
+  function newGame() {
+    
+  }
