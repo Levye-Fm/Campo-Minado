3701d1e96547058598c93957aff18f1f65b084a0
Levye-Fm <levi.ferreira08@aluno.ifce.edu.br>
2025-02-06 15:47:19 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // variavel cronometro tem que ser criada fora da função para evitar bug na hora de resetar o cronometro
 var cronometro;
-
+var espacos = [];
 
 function calcularTempo() {
 
@@ -8,14 +8,14 @@ function calcularTempo() {
     let minutos = 0;
     //span que representa aonde o cronometro está
     var tempo = document.getElementById('tempo');
+
     //para o cronometro(para caso um cronometro já esteja rodando e o usuário comece outro jogo, reseta o cronometro)
     clearInterval(cronometro);
 
-    //a cada 1000 milissegundos, aumenta um segundo e atualiza o span do cronometro
     cronometro = setInterval(function() {  
       segundos++;
       
-      if (segundos == 60) { // a cada 60 segundos, aumenta um minuto e volta os segundos para o valor 0
+      if (segundos == 60) {
         segundos = 0;
         minutos++
       }
@@ -39,22 +39,23 @@ function newGame() {
   run = false;
 }
 
-//codigo para criar os espacos onde estarão as bombas etc.
+//codigo para criar os espacos para cavar.
 function carregarEspacos() {
  
   var campo = document.getElementById('campo');
 
-  var espacos = [];
 
-  for (let i = 0; i<10; i++) {
+
+  for (let i = 0, k = 0; i<10; i++) {
     espacos[i] = [];
     for(let j = 0; j<12;j++) {
       espacos[i][j] = document.createElement('div');
-      espacos[i][j].setAttribute('class', 'dado')
+      espacos[i][j].setAttribute('class', 'dado');
       campo.appendChild(espacos[i][j]);
     }
   }
 }
+
 
 // codigo em comentario para não repetir bomba: 
 //var randomNum = Math.floor(Math.random()*10);
